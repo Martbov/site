884a4ce5ac0e90a5e66bb86a4e37f67e08e7bdee
Martbov <Mart@MacBook-Pro-van-Mart.local>
2015-04-09 14:55:40 +0200

    Log aangepast en menuverwijzing voor phpquiz aangepast

--- website/scripts/menu.js
+++ website/scripts/menu.js
@@ -4,6 +4,6 @@ document.write('<li><a href=\'photobook.html\'>Week 2</a></li>');
 document.write('<li><a href=\'quiz.html\'>Week 3</a></li>');
 document.write('<li><a href=\'clock.html\'>Week 4</a></li>');
 document.write('<li><a href=\'yahtzee.php\'>Week 5</a></li>');
-document.write('<li><a href=\'phpquiz.html\'>Week 6</a></li>');
+document.write('<li><a href=\'phpquiz.php\'>Week 6</a></li>');
 document.write('<li><a href=\'log.html\'>Logboek</a></li>');
 document.write('<li><a href=\'congres/main.html\'>Congres site</a></li>');
