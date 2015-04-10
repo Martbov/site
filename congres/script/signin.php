83062e0cf7147108792823963851b6fe10840331
Martbov <Mart@RN-145-97-166-148.eduroam.rug.nl>
2015-04-10 14:22:37 +0200

    wachtwoord verwijderd uit php

--- congres/script/signin.php
+++ congres/script/signin.php
@@ -4,7 +4,7 @@
  
  
 <?php
-$con = mysql_connect("mysql01.service.rug.nl","s2174634","Peizerweg21");
+$con = mysql_connect("mysql01.service.rug.nl","s2174634","password");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
